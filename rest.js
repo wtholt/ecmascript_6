c8d704d62728b9aa702fa2aaae888168e6c4de4e
William Holt <wtholt27@gmail.com>
2015-05-22 19:29:09 -0400

    learning es6

--- rest.js
+++ rest.js
@@ -1,9 +1,11 @@
-var args = process.argv[2]
+var args = process.argv[2];
 module.exports = function average(...args){
   var result = 0
-  args.forEach(function(value){
-
-
+  var avg = 0
+  var length = args.length
+  args.forEach(function (value){
+    result += value;
+    avg = result/length;
   });
-
+  return avg;
 }
